236347a597ec52266be35de0a11149d31431ebc9
baal <alburquenque.letelier@gmail.com>
2025-06-26 21:13:33 -0400

    5 consultas complejas

--- sql_querys.sql
+++ sql_querys.sql
@@ -34,15 +34,52 @@
 
 -- Consulta 3
 -- Todos los profesionales médicos especialidad Neurología que han atendido a más de 6 pacientes en el mes de abril
-SELECT pm.*, COUNT(DISTINCT cm.rut_paciente) as total_pacientes
-FROM profesionalmedico pm
-INNER JOIN control_medico cm ON pm.rut_profesionalmedico = cm.rut_profesionalmedico
-WHERE pm.especialidad = 'Neurología'
- AND MONTH(cm.fecha_asistencia) = 4
- AND cm.fecha_asistencia IS NOT NULL
-GROUP BY pm.rut_profesionalmedico
-HAVING COUNT(DISTINCT cm.rut_paciente) > 6
-INTO OUTFILE '/var/lib/mysql-files/consulta3.csv'
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
+-- SELECT pm.*, COUNT(DISTINCT cm.rut_paciente) as total_pacientes
+-- FROM profesionalmedico pm
+-- INNER JOIN control_medico cm ON pm.rut_profesionalmedico = cm.rut_profesionalmedico
+-- WHERE pm.especialidad = 'Neurología'
+--  AND MONTH(cm.fecha_asistencia) = 4
+--  AND cm.fecha_asistencia IS NOT NULL
+-- GROUP BY pm.rut_profesionalmedico
+-- HAVING COUNT(DISTINCT cm.rut_paciente) > 6
+-- INTO OUTFILE '/var/lib/mysql-files/consulta3.csv'
+-- FIELDS TERMINATED BY ',' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- Consulta 4
+-- Listar nombre y apellido de todos los doctores que han entregado más licencias que el promedio de licencias durante el año 2024
+-- SELECT pm.nombres, pm.ap_paterno, COUNT(l.folio) as total_licencias
+-- FROM profesionalmedico pm
+-- INNER JOIN control_medico cm ON pm.rut_profesionalmedico = cm.rut_profesionalmedico
+-- INNER JOIN licencia l ON cm.licencia_folio = l.folio
+-- WHERE YEAR(cm.fecha_asistencia) = 2024
+--  AND cm.fecha_asistencia IS NOT NULL
+-- GROUP BY pm.rut_profesionalmedico, pm.nombres, pm.ap_paterno
+-- HAVING COUNT(l.folio) > (
+--    SELECT AVG(licencias_por_doctor)
+--    FROM (
+--        SELECT COUNT(l2.folio) as licencias_por_doctor
+--        FROM profesionalmedico pm2
+--        INNER JOIN control_medico cm2 ON pm2.rut_profesionalmedico = cm2.rut_profesionalmedico
+--        INNER JOIN licencia l2 ON cm2.licencia_folio = l2.folio
+--        WHERE cm2.fecha_asistencia IS NOT NULL
+--        GROUP BY pm2.rut_profesionalmedico
+--    ) as subconsulta
+-- )
+-- INTO OUTFILE '/var/lib/mysql-files/consulta4.csv'
+-- FIELDS TERMINATED BY ',' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- Consulta 5
+-- Listar todos los pacientes que han tenido licencia medica el 2025
+-- SELECT DISTINCT * 
+-- FROM paciente p
+-- INNER JOIN control_medico cm ON p.rut_paciente = cm.rut_paciente
+-- INNER JOIN licencia l ON cm.licencia_folio = l.folio
+-- WHERE YEAR(cm.fecha_asistencia) = 2025
+-- INTO OUTFILE '/var/lib/mysql-files/consulta5.csv'
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
